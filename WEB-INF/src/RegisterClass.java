836faac8a80f09f0fa4c10bf5591bfca6b7fbaec
Thomasnolan350 <thomasnolan350@gmail.com>
2025-12-02 19:23:57 +0000

    Add files via upload

---
+++ WEB-INF/src/RegisterClass.java
@@ -0,0 +1,38 @@
+
+public class RegisterClass {
+	
+	//declare variables of the same name as the webpage paramters taken in
+	private String userName;
+	private String password;
+
+	public RegisterClass() {
+
+	}
+	
+	public String registration() {
+		return "SUCCESS";
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+
+	
+
+}
